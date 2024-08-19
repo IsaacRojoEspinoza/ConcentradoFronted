c94f5898f261d507b93880c437741267b073d4fd
ROJO ESPINOZA MARIO ISAAC <mario.rojo@ine.mx>
2024-08-19 16:57:45 -0600

    menu principal

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -9,6 +9,7 @@ import { useEffect } from 'react';
 import axios from 'axios'
 import { useLocation } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import { cardData } from './datos';
 
 export const Home = () => {
   // const location = useLocation();
@@ -38,132 +39,21 @@ export const Home = () => {
 
   return (
     <Layout>
-      <div className="container mt-3 mb-3">
-        <div className={"col-md"}>
-          <div className={"row  mt-3 pt-3"}>
-            <div className={"col-sm-3 mb-3 mb-sm-0"}>
-              <div className={"card home_card"}>
-                <div className={"card-body "}>
-                  <div className={"card-text cardText"}>Total de Entregables</div>
-                  <div className={"d-flex align-items-center px-3 pt-3"}>
-                    <div className={"card-text_total me-3"}><h1>{taskData.completed_task_count + taskData.ongoing_task_count + taskData.failed_count}</h1></div>
-                    <div><FeatherIcon className={"home-action-icons ms-5 increased-size text-primary"} icon={"file-text"} /></div>
-                  </div>
-                </div>
+      <div className="row row-cols-1 row-cols-md-4 g-4">
+        {cardData.map((card, index) => (
+          <div className="col d-flex justify-content-center" key={index}>
+            <div className="card">
+              <div className="card-img-wrapper">
+                <img src={card.imgSrc} className="card-img-top" alt={`Card ${index + 1}`} />
               </div>
-            </div>
-            <div className={"col-sm-3 mb-3 mb-sm-0"}>
-              <div className={"card home_card"}>
-                <div className={"card-body p-3"}>
-                  <div className={"card-text cardText"}>Total de Entregables (Concluidos)</div>
-                  <div className={"d-flex align-items-center px-5 pt-3"}>
-                    <div className={"card-text_total me-5"}><h1>{taskData.completed_task_count}</h1></div>
-                    <div><FeatherIcon className={"home-action-icons ms-5 increased-size text-success"} icon={"user-check"} /></div>
-                  </div>
-                </div>
+              <div className="card-body">
+                <h5 className="card-title">{card.title}</h5>
+                <p className="card-text">{card.text}</p>
               </div>
-            </div>
-            <div className={"col-sm-3 mb-3 mb-sm-0"}>
-              <div className={"card home_card"}>
-                <div className={"card-body p-3"}>
-                  <div className={"card-text cardText"}>Total de Entregables (Restantes)</div>
-                  <div className={"d-flex align-items-center px-5 pt-3"}>
-                    <div className={"card-text_total me-5"}><h1>{taskData.ongoing_task_count}</h1></div>
-                    <div><FeatherIcon className={"home-action-icons ms-5 increased-size text-dark"} icon={"trending-up"} /></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* <div className={"col-sm-3 mb-3 mb-sm-0"}>
-              <div className={"card home_card"}>
-                <div className={"card-body "}>
-                  <div className={"card-text cardText"}>Failed Tasks</div>
-                  <div className={"d-flex align-items-center px-5 pt-3"}>
-                    <div className={"card-text_total me-5"}><h1>{taskData.failed_count}</h1></div>
-                    <div><FeatherIcon className={"home-action-icons ms-5 increased-size text-danger"} icon={"user-x"} /></div>
-                  </div>
-                </div>
-              </div>
-            </div> */}
-          </div>
-
-          <div className="row m-1 p-3 mt-4">
-            <h1 className="p-3 heading">Entregables por mes</h1>
-            <div className="default-container">
-              <Chart
-                options={{
-                  colors: [
-                    '#ab37e7', '#6b0d0d', '#546E7A', '#E91E63', '#FF9800'
-                  ],
-                  chart: {
-                    id: "basic-bar"
-                  },
-                  xaxis: {
-                    categories: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
-                  }
-                }}
-                series={[
-                  {
-                    name: "Entregables Concluidos",
-                    data: [
-                      taskData.graph && taskData.graph[1] ? taskData.graph[1].completed : 0,
-                      taskData.graph && taskData.graph[2] ? taskData.graph[2].completed : 0,
-                      taskData.graph && taskData.graph[3] ? taskData.graph[3].completed : 0,
-                      taskData.graph && taskData.graph[4] ? taskData.graph[4].completed : 0,
-                      taskData.graph && taskData.graph[5] ? taskData.graph[5].completed : 0,
-                      taskData.graph && taskData.graph[6] ? taskData.graph[6].completed : 0,
-                      taskData.graph && taskData.graph[7] ? taskData.graph[7].completed : 0,
-                      taskData.graph && taskData.graph[8] ? taskData.graph[8].completed : 0,
-                      taskData.graph && taskData.graph[9] ? taskData.graph[9].completed : 0,
-                      taskData.graph && taskData.graph[10] ? taskData.graph[10].completed : 0,
-                      taskData.graph && taskData.graph[11] ? taskData.graph[11].completed : 0,
-                      taskData.graph && taskData.graph[12] ? taskData.graph[12].completed : 0,
-
-                    ]
-                  },
-                  {
-                    name: "Entregables Pendientes",
-                    data: [
-                      taskData.graph && taskData.graph[1] ? taskData.graph[1].running : 0,
-                      taskData.graph && taskData.graph[2] ? taskData.graph[2].running : 0,
-                      taskData.graph && taskData.graph[3] ? taskData.graph[3].running : 0,
-                      taskData.graph && taskData.graph[4] ? taskData.graph[4].running : 0,
-                      taskData.graph && taskData.graph[5] ? taskData.graph[5].running : 0,
-                      taskData.graph && taskData.graph[6] ? taskData.graph[6].running : 0,
-                      taskData.graph && taskData.graph[7] ? taskData.graph[7].running : 0,
-                      taskData.graph && taskData.graph[8] ? taskData.graph[8].running : 0,
-                      taskData.graph && taskData.graph[9] ? taskData.graph[9].running : 0,
-                      taskData.graph && taskData.graph[10] ? taskData.graph[10].running : 0,
-                      taskData.graph && taskData.graph[11] ? taskData.graph[11].running : 0,
-                      taskData.graph && taskData.graph[12] ? taskData.graph[12].running : 0,
-                    ]
-                  },
-                  {
-                    name: "Entegables Finalizados",
-                    data: [
-                      taskData.graph && taskData.graph[1] ? taskData.graph[1].failed : 0,
-                      taskData.graph && taskData.graph[2] ? taskData.graph[2].failed : 0,
-                      taskData.graph && taskData.graph[3] ? taskData.graph[3].failed : 0,
-                      taskData.graph && taskData.graph[4] ? taskData.graph[4].failed : 0,
-                      taskData.graph && taskData.graph[5] ? taskData.graph[5].failed : 0,
-                      taskData.graph && taskData.graph[6] ? taskData.graph[6].failed : 0,
-                      taskData.graph && taskData.graph[7] ? taskData.graph[7].failed : 0,
-                      taskData.graph && taskData.graph[8] ? taskData.graph[8].failed : 0,
-                      taskData.graph && taskData.graph[9] ? taskData.graph[9].failed : 0,
-                      taskData.graph && taskData.graph[10] ? taskData.graph[10].failed : 0,
-                      taskData.graph && taskData.graph[11] ? taskData.graph[11].failed : 0,
-                      taskData.graph && taskData.graph[12] ? taskData.graph[12].failed : 0,
-
-                    ]
-                  }
-                ]}
-                type="bar"
-                width="100%"
-              />
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </Layout>
-  )
-}
+  );
+};
