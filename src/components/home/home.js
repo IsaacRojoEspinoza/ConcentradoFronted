c2b659e738a592bea3bd6bb0fea7669fa63d1bda
ROJO ESPINOZA MARIO ISAAC <mario.rojo@ine.mx>
2024-08-20 18:38:20 -0600

    se agregan link al grid

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,24 +7,17 @@ import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { useEffect } from 'react';
 import axios from 'axios'
-import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { cardData } from './datos';
 
 export const Home = () => {
-  // const location = useLocation();
-  // const token = location?.state?.token;
   const token = Cookies.get('tokenCookie');
-  const [taskData, setTaskData] = useState([]);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://127.0.0.1:8000/home');
-        console.log(response.data);
-        setTaskData(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
       }
     };
 
@@ -37,18 +30,28 @@ export const Home = () => {
       <div className="row row-cols-1 row-cols-md-4 g-4">
         {cardData.map((card, index) => (
           <div className="col d-flex justify-content-center" key={index}>
-            <div className="card">
-              <div className="card-img-wrapper">
-                <img src={card.imgSrc} className="card-img-top" alt={`Card ${index + 1}`} />
+                   <Link
+            to="/avance"
+            state={{ entidad: card.entidad }} // Pasa el parámetro 'entidad' a través del estado
+            className="text-decoration-none"
+          >
+              <div className="card">
+                <div className="card-img-wrapper">
+                  <img src={card.imgSrc} className="card-img-top" alt={`Card ${index + 1}`} />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">{card.title}</h5>
+                  <p className="card-text">{card.text}</p>
+                </div>
               </div>
-              <div className="card-body">
-                <h5 className="card-title">{card.title}</h5>
-                <p className="card-text">{card.text}</p>
-              </div>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
     </Layout>
   );
+
+
+
+
 };
